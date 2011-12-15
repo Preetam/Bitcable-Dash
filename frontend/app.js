bb31c57629c7f5259d937764aa733415058f169c
root <root@dash.bitcable.com>
2011-12-15 09:55:01 -0500

    Express framework

---
+++ frontend/app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , crypto = require('crypto')
+  , fs = require('fs')
+
+
+// SSL features
+
+var privateKey = fs.readFileSync('privatekey.pem').toString();
+var certificate = fs.readFileSync('certificate.pem').toString();
+
+var app = module.exports = express.createServer({key: privateKey, cert: certificate});
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(443, '199.58.161.156');
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
