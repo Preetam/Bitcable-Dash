ae4914a424b79d580eb7aebdfc54f5455443945a
Preetam Jinka <xanic.cm@gmail.com>
2011-12-24 11:36:57 -0500

    Frontend additions

--- frontend/routes/manage.js
+++ frontend/routes/manage.js
@@ -1,7 +1,15 @@
+var db = require('../db.js').db;
+
 exports.manage = function(req, res){
 	console.log(req.session.user);
-	if(req.session && req.session.user)
-		res.render('manage');
+	console.log(req.params.kvmid);
+	var kvmid = req.params.kvmid.replace(/[^0-9]/g, '');
+	if(req.session && req.session.user) {
+		db.view('dash', 'getkvm', { key: parseInt(kvmid) }, function(e,r,h) {
+			console.log(r.rows[0].value);
+			res.render('manage', r.rows[0].value);
+		});
+	}
 	else
 		res.render('login');
 };
